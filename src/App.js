5e45f54647dd6db016140df7396a6e0fad728c98
KyleKeenanGMIT <kylekeenan@live.co.uk>
2023-12-06 21:34:19 +0000

    navbar added to app.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,27 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-//switch import
-import Records from './Records';//importing records.js
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'; // Import Bootstrap components
+import Records from './routes/Records';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css'; // bootstrap import
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar expand="lg" bg="body-tertiary" variant="dark">
-          <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+    <div>
+      {/* Bootstrap Navbar */}
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#">Guinness World Records</Navbar.Brand>
+          {/* You can add more navbar items here */}
+        </Container>
+      </Navbar>
 
-        <Switch>
-          <Route path="/records">
-            <Records />
-          </Route>
-          <Route path="/">
-            <h1>Welcome to the Home Page</h1>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+      {/* Main content */}
+      <Container>
+        <h1>Your App</h1>
+        <Records />
+      </Container>
+    </div>
   );
 }
 
