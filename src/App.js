9be0e8ce2d45cec9909bdd15cb5f731f79df92ca
KyleKeenanGMIT <kylekeenan@live.co.uk>
2023-12-06 17:20:35 +0000

    navbar imported from react website

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-//importing react and records file.
+// App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Records from './Records';
-
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'; // Import Bootstrap components
 
 function App() {
   return (
-    <div className="App">
-      <Records />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar expand="lg" bg="body-tertiary" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <Switch>
+          <Route path="/records">
+            <Records />
+          </Route>
+          <Route path="/">
+            {/* Your home component or content */}
+            <h1>Welcome to the Home Page</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;//export.
+export default App;
