865dc0df25507172f51b2ebfc8a4cd73aa4722bc
KyleKeenanGMIT <kylekeenan@live.co.uk>
2023-12-06 16:30:18 +0000

    records.js added to src folder.

---
+++ src/Records.js
@@ -0,0 +1,28 @@
+//improting usestate and useeffect from react
+import React, { useState, useEffect } from 'react';
+
+const Records = () => {
+    const [records, setRecords] = useState([]);
+
+    useEffect(() => {
+        // Fetching records.js from express server
+        fetch('http://localhost:4000/records')
+            .then((response) => response.json())
+            .then((data) => setRecords(data));
+    }, []);
+
+    return (
+        <div>{/*title and main div*/}
+            <h2>Guinness World Records</h2>
+            <ul>
+                {records.map((record) => (//map added with title, category and id of record.
+                    <li key={record._id}>
+                        {record.title} - {record.category}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Records;//export.
