d3449a8df54c94eed07445dd4653ec9224d3a859
KyleKeenanGMIT <kylekeenan@live.co.uk>
2023-12-14 21:47:06 +0000

    login page created with register/login

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const bcrypt = require('bcrypt');
-const User = require('./components/user')
+const User = require('./components/User');
 const PORT = process.env.PORT || 5000;
 
 
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'; // routes import
-import { Routes, Route , Link } from 'react-router-dom'; // router import
+import { Routes, Route , Link, Outlet } from 'react-router-dom'; // router import
 import Records from './routes/Records'; // records import
 import Create from './Create'; // crate.js import
 import Home from './routes/home'; // homepage import
@@ -7,6 +7,7 @@ import Container from 'react-bootstrap/Container'; // react container import
 import Navbar from 'react-bootstrap/Navbar'; // react navbar import
 import Nav from 'react-bootstrap/Nav'; // nav import
 import 'bootstrap/dist/css/bootstrap.min.css'; // bootstrap import
+import Login from './Login';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
                 <Nav.Link as={Link} to="/">Home</Nav.Link> {/* home title */}
                 <Nav.Link as={Link} to="/view-records">View Records</Nav.Link> {/* view existing records */}
                 <Nav.Link as={Link} to="/create">Add Record</Nav.Link> {/* create.js title */}
+                <Nav.Link as={Link} to="/login">Login/Signup</Nav.Link> {/* home title */}
               </Nav>
             </Navbar.Collapse>
           </Container>
@@ -28,9 +30,12 @@ function App() {
 
         {/* Route links in the navbar */}
         <Routes>
+          
+        <Route path="/login" element={<Login />} />{/* login.js */}
         <Route path="/" exact element={<Home />} /> {/* homepagee */}
         <Route path="/view-records" element={<Records />} /> {/* viewing records route */}
         <Route path="/create" element={<Create />} /> {/* crwting new records route */}
+        
         </Routes>
       </div>
     
