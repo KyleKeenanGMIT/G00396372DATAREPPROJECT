275ad1396163cd92c0e0948c61c26e2f3987ba3d
KyleKeenanGMIT <kylekeenan@live.co.uk>
2023-12-14 19:10:45 +0000

    adjusted mongoose schema

--- server/components/Record.js
+++ server/components/Record.js
@@ -1,13 +1,12 @@
-//record file setup. mongoose imported.
 const mongoose = require('mongoose');
 
-//records - adding a new one with title category and description of the record.
+// Define a schema
 const recordSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    category: { type: String, required: true },
-    description: { type: String },
+  title: String,
+  description: String,
 });
-//records are stored in the recordSchema variable.
+
+// Create a model
 const Record = mongoose.model('Record', recordSchema);
-//export record.js
-module.exports = Record;
+
+module.exports = Record; // Exporting record.js
