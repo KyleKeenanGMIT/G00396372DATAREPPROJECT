3aed865b6942e1d3264a448ea0ca4040e3564c42
KyleKeenanGMIT <kylekeenan@live.co.uk>
2023-12-14 20:46:36 +0000

    data is being uploaded to atlas

--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const mongoose = require('mongoose');//mongodb import cluster
+// Mongoose Schema and Model
+const recordSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  // Add other fields as needed
+});
 
-mongoose.connect('mongodb+srv://admin:admin@cluster0.hz2nopg.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })//connection string
+const Record = mongoose.model('Record', recordSchema);
+
+// MongoDB Connection
+mongoose.connect('mongodb+srv://admin:admin@cluster0.hz2nopg.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB', err));//error if unable to connect
+  .catch(err => console.error('Could not connect to MongoDB', err));
 
-
-// CORS handling
+// CORS Handling
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*'); // Update this in production
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
@@ -19,29 +27,20 @@ app.use((req, res, next) => {
 // Enabling express to parse JSON data
 app.use(express.json());
 
-// Array to store added records
-let records = [];
-
-// Route to send a greeting (example home route)
+// Route Handlers
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.post('/records', async (req, res) => {
   try {
-    const newRecord = new Record({
-      title: req.body.title,
-      description: req.body.description,
-     
-    });
-
+    const newRecord = new Record(req.body);
     await newRecord.save();
     res.status(201).send('Record added successfully');
   } catch (error) {
     res.status(500).send('Error saving record');
   }
 });
-
 
 app.get('/records', async (req, res) => {
   try {
@@ -52,9 +51,7 @@ app.get('/records', async (req, res) => {
   }
 });
 
-
-
-// Start the server
+// Start the Server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
