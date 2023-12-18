6c77d9a3fb1730af97cfe0386fca6d6649decbc6
KyleKeenanGMIT <kylekeenan@live.co.uk>
2023-12-18 23:00:17 +0000

    create.js styling

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'; //usestate import
+import React, { useState } from 'react'; // useState import
+import { Container, Form, Button } from 'react-bootstrap'; // Import Bootstrapp styling
 
 function Create() {
-  // Initial state to clear form after use.
+  // Initial state to clear form after record is typed in
   const initialState = {
     title: '',
     description: '',
@@ -9,15 +10,17 @@ function Create() {
 
   const [record, setRecord] = useState(initialState); // setting record
 
+  // Handles changes in the form fields
   const handleChange = (e) => {
     setRecord({ ...record, [e.target.name]: e.target.value });
-  };//changes
+  }; // changes
 
+  // Handles form submission to add a new record
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/records', {//sends to server
+      const response = await fetch('http://localhost:5000/records', { // sends to server
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -29,29 +32,43 @@ function Create() {
         throw new Error('error');
       }
 
-      // clearing the form
+      // Clearing the form after successful submission
       setRecord(initialState);
 
     } catch (error) {
       console.error('Failed to add record to database:', error);
-      // error to show record could not be added to database
+      // Error to show record could not be added to database
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Title</label>
-        <input type="text" name="title" value={record.title} onChange={handleChange} />
-      </div>
-      <div>
-        <label>Description</label>
-        <textarea name="description" value={record.description} onChange={handleChange}></textarea>
-      </div>
-      <button type="submit">Add Record</button>
-      {/* input fields to add a record */}
-    </form>
+    <Container className="mt-4">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Title</Form.Label>
+          <Form.Control 
+            type="text" 
+            name="title" 
+            value={record.title} 
+            onChange={handleChange} 
+            required 
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Description</Form.Label>
+          <Form.Control 
+            as="textarea" 
+            name="description" 
+            value={record.description} 
+            onChange={handleChange} 
+            required 
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">Add Record</Button>
+        {/* Input fields to add a record */}
+      </Form>
+    </Container>
   );
 }
 
-export default Create;//exporting create .js
+export default Create; // Exporting Create.js
