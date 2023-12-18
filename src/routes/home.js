974da259979ebb844c4c8eeba57569f584a49a9c
KyleKeenanGMIT <kylekeenan@live.co.uk>
2023-12-18 23:00:44 +0000

    adding student code

--- src/routes/home.js
+++ src/routes/home.js
@@ -7,7 +7,7 @@ function Home() {
       <Row className="justify-content-center">
         <Col md={8} className="text-center">
           <h1>Welcome to my Data Rep Project - Guinness World Records</h1>
-          <p className="mt-4">Made by Kyle Keenan</p>
+          <p className="mt-4">Made by Kyle Keenan G00396372</p>
           <p className="mt-4">My Project works by gathering JSON Data from a Guinness World Record API available on Google!
           <br></br>
           You are able to update the existing data, create new data & delete data. All this data is stored on my Database Mongo DB.</p>
