cd583bebcdff057d2d3485fce699c4d829a4ac90
KyleKeenanGMIT <kylekeenan@live.co.uk>
2023-12-18 22:55:02 +0000

    home page styling

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,14 +1,20 @@
-// home page
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap'; // Bootstrap import for styling
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to my Data Rep Project - Guiness World Records
-      </h1>
-      <p>Made by Kyle Keenan</p>
-      {/* displaying title on homepage */}
-    </div>
+    <Container className="mt-5">
+      <Row className="justify-content-center">
+        <Col md={8} className="text-center">
+          <h1>Welcome to my Data Rep Project - Guinness World Records</h1>
+          <p className="mt-4">Made by Kyle Keenan</p>
+          <p className="mt-4">My Project works by gathering JSON Data from a Guinness World Record API available on Google!
+          <br></br>
+          You are able to update the existing data, create new data & delete data. All this data is stored on my Database Mongo DB.</p>
+          {/* Displaying title on the homepage */}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
