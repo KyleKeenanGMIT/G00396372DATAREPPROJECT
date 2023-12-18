cb7dd8f80ded305865ba395b91f8dfdd0e6672f7
KyleKeenanGMIT <kylekeenan@live.co.uk>
2023-12-18 22:45:49 +0000

    styling using bootstrap

--- src/App.js
+++ src/App.js
@@ -9,58 +9,47 @@ import Nav from 'react-bootstrap/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Login';
 import PrivateRoute from './PrivateRoute';
-import Edit from './edit'; //edit imported
-
+import Edit from './edit';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const handleLogout = () => {
     setIsAuthenticated(false);
-    //sets the authentication of the user to false when logging out.
   };
 
   return (
     <Router>
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar bg="dark" variant="dark" expand="lg" className="nav-1">
         <Container>
-          <Navbar.Brand as={Link} to="/">
-            Guinness World Records
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link as={Link} to="/login">
-                Login/Signup
-              </Nav.Link>
-              {isAuthenticated && (
+          <Navbar.Brand as={Link} to="/">Guinness World Records</Navbar.Brand>
+          <Navbar.Toggle aria-controls="boostrap-navbar-nav" />
+          <Navbar.Collapse id="boot-navbar-nav">
+            <Nav className="nav-auto">
+              {isAuthenticated ? (
                 <>
-                  <Nav.Link as={Link} to="/">
-                    Home
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/view-records">
-                    View Records
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/create">
-                    Add Record
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/login" onClick={handleLogout}>
-                    Logout
-                    </Nav.Link>
+                  <Nav.Link as={Link} to="/">Home</Nav.Link>
+                  <Nav.Link as={Link} to="/view-records">View Records</Nav.Link>
+                  <Nav.Link as={Link} to="/create">Add Record</Nav.Link>
+                  <Nav.Link as={Link} to="/login" onClick={handleLogout}>Logout</Nav.Link>
                 </>
+              ) : (
+                <Nav.Link as={Link} to="/login">Login/Signup</Nav.Link>
               )}
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
 
-      <Routes>
-        <Route path="/login" element={<Login onAuthenticate={setIsAuthenticated} />} />
-        <Route path="/" element={<PrivateRoute isAuthenticated={isAuthenticated}><Home /></PrivateRoute>} />
-        <Route path="/view-records" element={<PrivateRoute isAuthenticated={isAuthenticated}><Records /></PrivateRoute>} />
-        <Route path="/create" element={<PrivateRoute isAuthenticated={isAuthenticated}><Create /></PrivateRoute>} />
-        <Route path="/edit/:id" element={<PrivateRoute isAuthenticated={isAuthenticated}><Edit /></PrivateRoute>} />
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path="/login" element={<Login onAuthenticate={setIsAuthenticated} />} />
+          <Route path="/" element={<PrivateRoute isAuthenticated={isAuthenticated}><Home /></PrivateRoute>} />
+          <Route path="/view-records" element={<PrivateRoute isAuthenticated={isAuthenticated}><Records /></PrivateRoute>} />
+          <Route path="/create" element={<PrivateRoute isAuthenticated={isAuthenticated}><Create /></PrivateRoute>} />
+          <Route path="/edit/:id" element={<PrivateRoute isAuthenticated={isAuthenticated}><Edit /></PrivateRoute>} />
+        </Routes>
+      </Container>
     </Router>
   );
 }
