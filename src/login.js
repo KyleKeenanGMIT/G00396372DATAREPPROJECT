2b8f5c3fa46146e1489d757bdd3c83eb18d89dd2
KyleKeenanGMIT <kylekeenan@live.co.uk>
2023-12-18 22:51:06 +0000

    login page styling

--- src/login.js
+++ src/login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
+import { Container, Form, Button, Row, Col } from 'react-bootstrap'; // Bootstrap imports
 
 function Login({ onAuthenticate }) {
   const [isLogin, setIsLogin] = useState(true);
@@ -7,12 +8,14 @@ function Login({ onAuthenticate }) {
     email: '',
     password: '',
   });
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
+  // Handles changes in the form fields
   const handleChange = (e) => {
     setUserCredentials({ ...userCredentials, [e.target.name]: e.target.value });
   };
 
+  // Handles form submission for login or registration
   const handleSubmit = async (e) => {
     e.preventDefault();
     const url = isLogin ? 'http://localhost:5000/login' : 'http://localhost:5000/register';
@@ -26,11 +29,8 @@ function Login({ onAuthenticate }) {
       });
       const data = await response.json();
       if (response.ok) {
-        console.log(data.message);
-
-        
-        onAuthenticate(true); 
-        navigate('/'); 
+        onAuthenticate(true);
+        navigate('/');
       } else {
         throw new Error(data.message);
       }
@@ -40,35 +40,41 @@ function Login({ onAuthenticate }) {
   };
 
   return (
-    <div>
-      <h2>{isLogin ? 'Login' : 'Register'}</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            value={userCredentials.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={userCredentials.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit">{isLogin ? 'Login' : 'Create Account'}</button>
-      </form>
-      <button onClick={() => setIsLogin(!isLogin)}>
-        {isLogin ? 'Need to create an account?' : 'Already have an account?'}
-      </button>
-    </div>
+    <Container className="login-0">
+      <Row className="justify-content-center">
+        <Col md={6}>
+          <h2 className="text-center">{isLogin ? 'Login' : 'Register'}</h2>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="login-1">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                value={userCredentials.email}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+            <Form.Group className="login-1">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                value={userCredentials.password}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit" className="b-001">
+              {isLogin ? 'Login' : 'Create Account'}
+            </Button>
+          </Form>
+          <Button variant="link" onClick={() => setIsLogin(!isLogin)} className="b-002">
+            {isLogin ? 'Need to create an account?' : 'Already have an account?'}
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
